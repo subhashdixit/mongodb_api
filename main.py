dab333b4d45541eb02a5e7f9daaba75375524953
Subhash Dixit <subhash.dixit@reckitt.com>
2026-02-21 16:49:40 +0530

    mongodb_api

---
+++ main.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from motor.motor_asyncio import AsyncIOMotorClient
+from bson import ObjectId
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+MONGO_URL = os.getenv("MONGO_URL")
+client = AsyncIOMotorClient(MONGO_URL) # Making APi more robust
+mongodb_db = client["gen_ai"]
+mongodb_collection = mongodb_db["api_testing"]
+
+app = FastAPI()
+
+class MongoDbAPITestingData(BaseModel):
+    name: str
+    phone: int
+    city: str
+    course: str
+
+@app.post("/mongodb/insert")
+async def mongodb_data_insert_helper(data: MongoDbAPITestingData): 
+# async is a not blocking function in a way that other processes will not be disturbed
+# await will make sure that it is not blocking any other functiuon, ofr this function it will wait for it's turn
+# motor is a driver like pymongo
+    reuslt = await mongodb_collection.insert_one(data.dict())
+    return {"message":f"Data inserted successfully at {str(reuslt.inserted_id)}"}
+
+def mongodb_get_data_helper(doc):
+    doc["id"] = str(doc["_id"])
+    del doc["_id"]
+    return doc
+
+@app.post("/mongodb/get_data")
+async def get_mongodb_data():
+    items = []
+    cursor = mongodb_collection.find({})
+    async for document in cursor:
+        items.append(mongodb_get_data_helper(document))
+    return items
+
+# To host this api on cloud, we need to push this code to repo then from repo to cloud so that the api can be accessible to anyone
+ 
